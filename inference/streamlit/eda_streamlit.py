58178147086aac5e2e4382ca9abf21ced4e062c8
Arina Chaplygina <arienachaplygina@yandex.ru>
2024-12-28 17:58:57 +0300

    functions have been moved to a separate file

--- inference/streamlit/eda_streamlit.py
+++ inference/streamlit/eda_streamlit.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
+from utils import get_top_10_heroes
+from plots import plot_metric_histogram, create_result_pie_chart, create_kills_bar_chart, create_deaths_plot,\
+    create_top_10_heroes_plot, create_selected_heroes_plot, create_box_plot, create_winrate_pie_chart,\
+    create_duration_histogram, create_kda_scatter_plot
 
 def run_eda_streamlit():
     st.title("Предсказательные модели для Dota 2")
@@ -20,8 +20,6 @@ def run_eda_streamlit():
     if upload_file is not None:
 
         df = pd.read_csv(upload_file)
-        #df['match_id'] = df['match_id'].astype('object')
-        #df['account_id'] = df['account_id'].astype('object')
 
         # Основная информация о датасете
         def display_dataset_info(df):
@@ -83,41 +81,12 @@ def run_eda_streamlit():
                     'Золото в минуту': 'gold_per_min',
                     'Опыт в минуту': 'xp_per_min'
                 }
-
                 selected_metric = st.selectbox("Выберите метрику для отображения", options=list(columns_to_plot.keys()))
-                selected_column = columns_to_plot[selected_metric]
-
-                sorted_data = player_data.sort_values(by='match_id')
-
-                fig12 = px.histogram(
-                    sorted_data,
-                    x='match_id',
-                    y=selected_column,
-                    labels={
-                        'match_id': 'Номер матча',
-                        selected_column: selected_metric
-                    },
-                    nbins=len(player_data['match_id'].unique()),
-                )
-
-                fig12.update_layout(
-                    bargap=0,
-                    xaxis_title='Номер матча',
-                    yaxis_title=selected_metric
-                )
+                fig12 = plot_metric_histogram(player_data, columns_to_plot, selected_metric)
                 st.plotly_chart(fig12)
 
-
-
                 st.write("#### Распределение исходов матчей для данного игрока")
-                result_table = pd.DataFrame({
-                    'Результат': ['Победы', 'Поражения'],
-                    'Количество': [player_data['win'].sum(), len(player_data) - player_data['win'].sum()]
-                })
-
-                fig13 = px.pie(result_table, names='Результат', values='Количество',
-                             color_discrete_sequence=['#FFB6C1', '#A3D9FF'],
-                             hole=0)
+                fig13 = create_result_pie_chart(player_data)
                 st.plotly_chart(fig13)
 
         # Статистика по выбранному матчу
@@ -158,38 +127,21 @@ def run_eda_streamlit():
                 st.write(detailed_stats)
 
                 st.write("#### Количество убийств по игрокам (в разрезе героев):")
-                fig_kills = px.bar(
-                    df_players,
-                    x='account_id',
-                    y='kills',
-                    color='hero_name',
-                    labels={'account_id': 'ID игрока', 'kills': 'Убийства', 'hero_name': 'Имя героя'}
-                )
-                fig_kills.update_layout(
-                    xaxis={'categoryorder': 'category ascending'},
-                    bargap=0,
-                    bargroupgap=0
-                )
+                fig_kills = create_kills_bar_chart(df_players)
                 st.plotly_chart(fig_kills)
 
                 st.write("#### Смерти по игрокам (в разрезе героев):")
-                fig_deaths = px.bar(df_players, x='account_id', y='deaths', color='hero_name',
-                                    labels={'account_id': 'ID игрока', 'deaths': 'Смерти', 'hero_name': 'Имя героя'})
+                fig_deaths = create_deaths_plot(df_players)
                 st.plotly_chart(fig_deaths)
 
         # Построение графиков по историческим данным
         def display_graphics(df):
             st.write("### Общая аналитика исторических данных")
             with st.expander("### Посмотреть информацию"):
-                hero_counts = df["hero_name"].value_counts()
-                top_10_heroes = hero_counts.head(10).index.tolist()
+                top_10_heroes = get_top_10_heroes(df)
 
                 st.write("#### Самые популярные герои")
-                df_top_10 = hero_counts[top_10_heroes].reset_index()
-                df_top_10.columns = ['hero_name', 'count']
-                df_top_10 = df_top_10.sort_values(by='count', ascending=False)
-                fig10 = px.bar(df_top_10, x='hero_name', y='count', color='hero_name',
-                               labels={'hero_name': 'Имя героя', 'count': 'Количество'})
+                fig10 = create_top_10_heroes_plot(df, top_10_heroes)
                 st.plotly_chart(fig10)
 
                 st.write("#### Статистика по топ-10 популярным героям")
@@ -208,81 +160,30 @@ def run_eda_streamlit():
 
                 st.write("#### Распределение убийств по героям")
                 selected_heroes = st.multiselect("Выберите героев для отображения (по умолчанию - 10 самых популярных)",
-                                                 options=hero_counts.index.tolist(),
+                                                 options=df['hero_name'].tolist(),
                                                  default=top_10_heroes)
 
                 filtered_df = df[df["hero_name"].isin(selected_heroes)]
 
-                fig1 = px.histogram(filtered_df, x="hero_name", y="kills", color="hero_name",
-                                    labels={"hero_name": "Имя героя", "kills": "Количество убийств"},
-                                    height=600)
-
-                fig1.update_layout(xaxis_title="Имя героя", yaxis_title="Количество убийств")
+                fig1 = create_selected_heroes_plot(filtered_df)
                 st.plotly_chart(fig1)
 
                 st.write("#### Box-plot распределения убийств по выбранным героям")
-                fig2 = px.box(filtered_df, x="hero_name", y="kills", color="hero_name",
-                              labels={"hero_name": "Имя героя", "kills": "Количество убийств"},
-                              height=600)
-                fig2.update_layout(xaxis_title="Имя героя", yaxis_title="Количество убийств")
+                fig2 = create_box_plot(filtered_df)
                 st.plotly_chart(fig2)
 
-                st.write("#### Топ-10 признаков сильнее всего коррелирующие с результатом матча")
-                numeric_df = df.select_dtypes(include=[np.number])
-                correlation_matrix = numeric_df.corr()
-                correlation_with_win = correlation_matrix['win']
-
-                sorted_correlations = correlation_with_win.abs().sort_values(ascending=False)
-                top_10_features = sorted_correlations.index[:10]
-
-                top_10_corr_matrix = correlation_matrix.loc[top_10_features, top_10_features]
-
-                fig, ax = plt.subplots(figsize=(12, 10))
-                sns.heatmap(
-                    top_10_corr_matrix,
-                    annot=True,
-                    fmt=".2f",
-                    cmap="viridis",
-                    cbar=True,
-                    ax=ax
-                )
-                st.pyplot(fig)
 
                 st.write("#### Победы команд Radiant vs Dire")
-                df['winrate'] = df['win'].mean()
-                radiant_wins = df[df['isRadiant'] == 1]['win'].sum()
-                dire_wins = df[df['isRadiant'] == 0]['win'].sum()
-
-                win_data = pd.DataFrame({
-                    'Команда': ['Radiant', 'Dire'],
-                    'Победы': [radiant_wins, dire_wins]
-                })
-                fig_pie = px.pie(win_data, names='Команда', values='Победы')
+                fig_pie = create_winrate_pie_chart(df)
                 st.plotly_chart(fig_pie)
 
                 st.write("#### Зависимость длительности матча на результат")
-                mapping = {0: 'Поражение', 1: 'Победа'}
-                df['win'] = df['win'].replace(mapping)
-
-                fig6 = px.histogram(
-                    df,
-                    x='duration',
-                    color='win',
-                    nbins=50,
-                    labels={'duration': 'Длительность матча', 'win': 'Результат'},
-                    barmode='overlay',
-                    color_discrete_map={'Поражение': 'lightcoral', 'Победа': 'lightblue'},
-                    category_orders={'win': ['Поражение', 'Победа']}
-                )
-                fig6.update_xaxes(title_text='Длительность матча')
-                fig6.update_yaxes(title_text='Количество матчей')
-                st.plotly_chart(fig6)
+                fig = create_duration_histogram(df)
+                st.plotly_chart(fig)
 
                 st.write("#### Соотношение KDA и убийств в минуту")
-                fig3 = px.scatter(df, x='kda', y='kills_per_min', color='win',
-                                  labels={'kda': 'KDA', 'kills_per_min': 'Убийств в минуту', 'win': 'Результат'})
-                fig3.update_layout(legend_title='Результат')
-                st.plotly_chart(fig3)
+                fig = create_kda_scatter_plot(df)
+                st.plotly_chart(fig)
 
         display_dataset_info(df)
         st.write('---')
