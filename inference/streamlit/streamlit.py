9c0b460adaa929c075f39b30d24c0e685ef941ee
ilyager99 <gerilovich.ip@gmail.com>
2024-12-31 01:29:27 +0400

    добавил логирование

--- inference/streamlit/streamlit.py
+++ inference/streamlit/streamlit.py
@@ -4,7 +4,11 @@ from fit import fit_model
 from predict import predict_model
 from model_info import display_model_info
 from eda_streamlit import run_eda_streamlit
+import logging
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def main():
     # Инициализация API
@@ -12,6 +16,8 @@ def main():
     port = 8000  # Замените на рабочий порт
     models_api_client = ModelsAPIClient(host, port)
     data_api_client = DataAPIClient(host, port)
+
+    logger.info("Инициализация API клиентов завершена.")
 
     # Заголовок приложения
     st.title("Модель по анализу данных")
@@ -26,12 +32,16 @@ def main():
     st.sidebar.header("Меню быстрого доступа")
     if st.sidebar.button("📊 EDA"):  # Кнопка EDA
         st.session_state.page = "📊 EDA"
+        logger.info("Пользователь выбрал страницу EDA.")
     if st.sidebar.button("🔄 Обучение модели"):
         st.session_state.page = "🔄 Обучение модели"
+        logger.info("Пользователь выбрал страницу обучения модели.")
     if st.sidebar.button("ℹ️ Информация о модели"):
         st.session_state.page = "ℹ️ Информация о модели"
+        logger.info("Пользователь выбрал страницу информации о модели.")
     if st.sidebar.button("🔮 Предсказания"):
         st.session_state.page = "🔮 Предсказания"
+        logger.info("Пользователь выбрал страницу предсказаний.")
 
     # Определение действия на основе текущей страницы
     if st.session_state.page == "📊 EDA":
@@ -43,7 +53,5 @@ def main():
     elif st.session_state.page == "ℹ️ Информация о модели":
         display_model_info(models_api_client)
 
-
 if __name__ == "__main__":
     main()
-
